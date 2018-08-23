80c5d649d38dc6c26473e53158fffe4eb1be89cf
yangdan <942652559@qq.com>
2018-08-23 15:39:37 +0800

    update

--- gusse-number.py
+++ gusse-number.py
@@ -5,7 +5,8 @@ Created on Wed Aug  8 14:39:12 2018
 @author: windows7
 """
 import random
-print ("猜0-20之间的整数")
+print("游戏开始啦！")
+print ("请猜0-20之间的整数")
 
 x=random.randint(0,20)#(最小值，最大值)
 count=0
@@ -13,10 +14,15 @@ while 1:
     count+=1
     number=int(input("请输入数字："))
     if number==x:
-        print("猜对啦！一共猜了",count,"次哦")
-        print("游戏结束！")
+        if count==1:
+            print("逆天！一次猜中！")
+        elif 1<count<=3:
+            print(count,"次就猜中了，佩服佩服")
+        else:
+            print("一共猜了",count,"次哦！")
         break
     elif number>x:
         print("猜大了")      
     elif number<x:
         print("猜小了")
+print("游戏结束！")
