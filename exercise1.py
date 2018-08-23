e0132c5d0b94926d2a30ede8f1cd94aef636af77
yangdan <942652559@qq.com>
2018-08-23 17:41:21 +0800

    自动写文件

--- exercise1.py
+++ exercise1.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ye# -*- coding: utf-8 -*-
 """
 Spyder Editor
 
@@ -134,3 +134,13 @@ print(x[0][1])
 print(x[1][0])
 
 
+import sys         # 引入 sys 模块
+ 
+list=[1,2,3,4]
+it = iter(list)    # 创建迭代器对象
+ 
+while True:
+    try:
+        print (next(it))
+    except StopIteration:
+        sys.exit()
--- reptile.py
+++ reptile.py
@@ -6,5 +6,17 @@ Created on Tue Aug  7 08:40:52 2018
 """
 #方法1
 import urllib2
+#直接请求
 response=urllib2.urloppen("")
+
+#获取状态码，200表示成功
 print (response.getcode())
+
+#读取内容
+cont=response.read()
+
+
+#方法2
+import urllib2
+#创建Request对象
+request=urllib2.Request(url)
