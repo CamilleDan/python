21a2f1a1b2f4562bb1e4184cab0598c8bafdfa66
yangdan <942652559@qq.com>
2018-09-06 11:37:13 +0800

    第一篇爬虫文档，可惜不能用了，因为安装python之前没有编译安装什么什么SSL库，所以python不支持了，哎，难过

--- reptile.py
+++ reptile.py
@@ -5,18 +5,38 @@ Created on Tue Aug  7 08:40:52 2018
 @author: windows7
 """
 #方法1
-import urllib2
-#直接请求
-response=urllib2.urloppen("")
+from urllib import request
+import re#使用正则表达式
 
-#获取状态码，200表示成功
-print (response.getcode())
+def getResponse(url):
+    #请求对象Request是一个类
+    url_request=request.Request(url)
+    url_response=request.urlopen(url_request)
+    
+    return url_response
+#直接请求获取网页内容，将内容赋予response对象
 
-#读取内容
-cont=response.read()
+def getJpg(data):
+    jpglist=re.findall(r'src="http.+?.jpg"',data)
+    return jpglist
 
+def downLoad(jpgUrl,n):
+    try:
+        request.urlretrieve(jpgUrl,'%s.jpg' %n)
+    except Exception as e:
+        print(e)
+    finally:
+        print('图片%s下载操作完成' %n)
+        
 
-#方法2
-import urllib2
-#创建Request对象
-request=urllib2.Request(url)
+http_response=getResponse('https://www.douban.com/')
+data=http_response.read().decode('utf-8')
+
+global n
+n=1
+L=getJpg(data)
+for jpginfo in L:
+    print(jpginfo)
+    s=re.findall(r'src="http.+?.jpg"',jpginfo)
+    downLoad(s[0],n)
+    n=n+1
